5438356c23d1cac19da6d4444337ee9015524233
Jessicarrr <JWP01993@gmail.com>
2020-04-25 16:50:16 +1000

    add custom middleware
    - This is for future use to assign every user a session ID if they don't already have one.

---
+++ Video Syncer/Middleware/CustomMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Video_Syncer.Middleware
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            System.Diagnostics.Debug.WriteLine("custom middleware is called");
+
+            // Call the next delegate/middleware in the pipeline
+            await _next(context);
+        }
+    }
+}
--- Video Syncer/Startup.cs
+++ Video Syncer/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Video_Syncer.Middleware;
 
 namespace Video_Syncer
 {
@@ -39,6 +40,7 @@ namespace Video_Syncer
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseMiddleware<CustomMiddleware>();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
