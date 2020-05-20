ab0cce697cac8f494a7262647e666a1c1f5b3d00
Jessicarrr <JWP01993@gmail.com>
2020-05-20 15:20:22 +1000

    add CTrace class for better Trace logging
    CTrace simply calls on Trace and automatically adds the tag [VSY] as well as the current date and time.

---
+++ Video Syncer/logging/CTrace.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Video_Syncer.logging
+{
+    public class CTrace
+    {
+        protected static readonly string tag = "[VSY]";
+
+        public static void WriteLine(string message)
+        {
+            Trace.WriteLine(tag + " " + GetFormattedDateTime() + ": " + message);
+        }
+
+        public static void TraceInformation(string message)
+        {
+            Trace.TraceInformation(tag + " " + GetFormattedDateTime() + ": " + message);
+        }
+
+        public static void TraceWarning(string message)
+        {
+            Trace.TraceWarning(tag + " " + GetFormattedDateTime() + ": " + message);
+        }
+
+        public static void TraceError(string message)
+        {
+            Trace.TraceError(tag + " " + GetFormattedDateTime() + ": " + message);
+        }
+
+        protected static string GetFormattedDateTime()
+        {
+            return DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+        }
+    }
+}
