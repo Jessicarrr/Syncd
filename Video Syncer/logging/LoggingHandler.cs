6038eac077921ac6b1234ac186172ce7e4b59ee5
Jessicarrr <JWP01993@gmail.com>
2020-05-29 15:47:41 +1000

    create proper loggingfactory in LoggingHandler.cs that works with azure

--- Video Syncer/logging/LoggingHandler.cs
+++ Video Syncer/logging/LoggingHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -9,7 +10,19 @@ namespace Video_Syncer.logging
 {
     public class LoggingHandler
     {
-        public static ILoggerFactory LoggerFactory { get; } = new LoggerFactory();
+        public static ILoggerFactory LoggerFactory { get; } = Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
+        {
+            //builder.AddConfiguration(Configuration.GetSection("Logging"));
+            builder.AddConsole();
+            builder.AddDebug();
+            builder.AddAzureWebAppDiagnostics();
+            builder.AddApplicationInsights("ikey");
+        });
+
+        public static ILogger CreateLogger2<T>()
+        {
+            return LoggerFactory.CreateLogger<T>();
+        }
 
         public static ILogger CreateLogger<T>()
         {
