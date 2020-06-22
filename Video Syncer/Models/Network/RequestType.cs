21b62adaff456c9791ebb1963a9923e760c52844
Jessicarrr <JWP01993@gmail.com>
2020-06-22 15:37:05 +1000

    move RequestType back to network folder
    - I accidentally moved RequestType to the OldNetworkingCode folder along with all the other files in that folder. RequestType should have stayed.

---
+++ Video Syncer/Models/Network/RequestType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Video_Syncer.Models.Network
+{
+    public enum RequestType
+    {
+        Join = 1,
+        ChangeVideoState = 2,
+        Leave = 3,
+        ChangeName = 4,
+        Kick = 5,
+        Ban = 6,
+        MakeAdmin = 7,
+        RearrangePlaylist = 8,
+        PlayPlaylistVideo = 9,
+        PlayVideo = 10,
+        RemoveFromPlaylist = 11,
+        AddToPlaylist = 12,
+        TimeChange = 13
+    }
+}
