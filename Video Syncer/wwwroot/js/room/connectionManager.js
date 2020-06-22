8759d9b284727cd75127e9091e9f77b47cde6c54
Jessicarrr <JWP01993@gmail.com>
2020-06-22 15:19:34 +1000

    add RequestType enums
    - Added enums (both client and server side) that encompass request types, this is a way for the client to tell the server what request it wants to make.

--- Video Syncer/wwwroot/js/room/connectionManager.js
+++ Video Syncer/wwwroot/js/room/connectionManager.js
@@ -4,7 +4,18 @@ var userId = -1;
 const RequestType = Object.freeze(
     {
         "Join": 1,
-        "ChangeVideoState": 2
+        "ChangeVideoState": 2,
+        "Leave": 3,
+        "ChangeName": 4,
+        "Kick": 5,
+        "Ban": 6,
+        "MakeAdmin": 7,
+        "RearrangePlaylist": 8,
+        "PlayPlaylistVideo": 9,
+        "PlayVideo": 10,
+        "RemoveFromPlaylist": 11,
+        "AddToPlaylist": 12,
+        "TimeChange": 13
     });
 
 function setupNetworking() {
