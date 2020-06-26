22600cd89079b28b3e2b809a5096f7251f61273c
Jessicarrr <JWP01993@gmail.com>
2020-06-26 14:53:22 +1000

    add IConnectionManager and ConnectionManager
    ConnectionManager will be a dependency injection in Room.cs. This class will be responsible for handling the connections for a room, and sending all users in a given room updates.

---
+++ Video Syncer/Models/Network/Room/Interface/IConnectionManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Video_Syncer.Models.Network.Room.Interface
+{
+    public interface IConnectionManager
+    {
+    }
+}
