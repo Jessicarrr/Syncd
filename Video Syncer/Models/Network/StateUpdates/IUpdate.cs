95c7b0e3e1bfb6ec48d711cf69fe5d2ff02942e6
Jessicarrr <JWP01993@gmail.com>
2020-06-26 14:52:34 +1000

    add IUpdate interface
    A class that will be sent to users to update them on room changes. The payload can include classes from Network.Payload.StateUpdates

---
+++ Video Syncer/Models/Network/StateUpdates/IUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Video_Syncer.Models.Network.StateUpdates
+{
+    public interface IUpdate
+    {
+        UpdateType updateType
+        {
+            get;
+            set;
+        }
+        Object payload
+        {
+            get;
+            set;
+        }
+    }
+}
